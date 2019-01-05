90ff38dc5a695961cb64cc0464191ed53d06e43f
YYD <yyd-yyd@hotmail.com>
2019-01-05 09:58:56 +0800

    remove `event-stream` npm package;

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,8 +11,8 @@ module.exports = {
     title: 'AIVision',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
